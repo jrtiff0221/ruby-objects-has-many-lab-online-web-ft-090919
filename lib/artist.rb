b47b33b40b3c9db1d78063cefa1ef20666b403da
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-24 02:19:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,24 +5,9 @@ class Artist
     @name = name 
   end
   
-   def add_song(song)
-    song.artist = self
-  end
- 
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-
-  def add_song_by_name(name)
-  song = Song.new(name)
-    add_song(song)
-  end
-  
-  def self.song_count
-    Song.all.length
-  
-  end
-    
+   def posts 
+   
+   end
 end
   
   
